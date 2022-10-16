0d17c67136767588fc8107ca7773387831975195
abs053 <batalla.alejandro@gmail.com>
2022-10-16 14:40:28 +0200

    SOundManager

--- Assets/Scripts/Sound/SoundManager.cs
+++ Assets/Scripts/Sound/SoundManager.cs
@@ -44,4 +44,14 @@ public class SoundManager : MonoBehaviour
         }
         s.source.Play();
     }
+
+    public void Stop(string name){
+        Sound s = Array.Find(sounds, sound => sound.name == name);
+        Debug.Log("No longer playing Sound " + name);
+        if(s == null){
+            Debug.LogWarning("Sound "+ name + " not found.");
+            return;
+        }
+        s.source.Stop();
+    }
 }
